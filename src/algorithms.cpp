da62cf7f662a788f8fbe3c517d717e73cb4ac641
Calum Kerr <calumxkerr@gmail.com>
2025-10-28 15:50:59 +0000

    Added starting logic for the DataCleaner

--- src/algorithms.cpp
+++ src/algorithms.cpp
@@ -4,7 +4,26 @@
 #include <set>
 
 std::vector<std::vector<std::string>> DataCleaner::parseCSV(const std::string& data){
-
+    /**
+     * in this functio i will take the data and parse it into a 2d vector of strings by
+     * splitting the data on the newline character and then splitting each line on the comma character because
+     * the data is in the scv format when a user uploads a file it is read as a string.
+     */
+    std::vector<std::vector<std::string> result;
+    std::stringstream ss(data);
+    std::string line;
+    while(std::getline(ss,line)){
+        std::vector<std::string> row;
+        std::stringstream lineStream(line);
+        std::string cell;
+        while(std::getline(lineStream,cell,',')){
+            row.push_back(cell);
+        }
+        if(!row.empty()){
+            result.push_back(row);
+        }
+    }
+    return result;
 }
 
 std::vector<std::vector<bool>> DataCleaner::detectMissingValues(const std::vector<std::vector<std::string>>& data){
