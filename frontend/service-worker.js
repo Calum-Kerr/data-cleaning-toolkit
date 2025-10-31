1001ef3c7ed2071c6d15dbc3aa22bb4f598737ea
Calum Kerr <calumxkerr@gmail.com>
2025-10-31 16:48:19 +0000

    Added a service worker so its truely offline

---
+++ frontend/service-worker.js
@@ -0,0 +1,4 @@
+const cacheName='data-cleaner-cache-v1';
+const urlsToCache=['/app','/algorithms.js','/algorithms.wasm','manifest.json'];
+self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(urlsToCache)))});
+self.addEventListener('fetch',event=>{event.respondWith(caches.match(event.request).then(response=>response||fetch(event.request)))});
