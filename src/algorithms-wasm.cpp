856877fff29584b17a5b4ba8e210620d1ff1eecc
Calum Kerr <calumxkerr@gmail.com>
2025-10-31 14:15:41 +0000

    Same as orignal but for compat

--- src/algorithms-wasm.cpp
+++ src/algorithms-wasm.cpp
@@ -5,6 +5,20 @@
 #include <emscripten/emscripten.h>
 
 std::vector<std::vector<std::string>> parseCSVInternal(const std::string& data){
-
+    std::vector<std::vector<std::string>> result;
+    std::stringstream ss(data);
+    std::string line;
+    while(std::getline(ss,line)){
+        std::vector<std::string> row;
+        std::stringstream lineStream(line);
+        std::string cell;
+        while(std::getline(lineStream,cell,',')){
+            row.push_back(cell);
+        }
+        if(!row.empty()){
+            result.push_back(row);
+        }
+    }
+    return result;
 }
 
