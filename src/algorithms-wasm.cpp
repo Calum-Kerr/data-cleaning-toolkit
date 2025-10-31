f41aa1d01ebf773df1767e7ff48ffdaccce15a60
Calum Kerr <calumxkerr@gmail.com>
2025-10-31 14:17:45 +0000

    To parse csv data

--- src/algorithms-wasm.cpp
+++ src/algorithms-wasm.cpp
@@ -22,3 +22,12 @@ std::vector<std::vector<std::string>> parseCSVInternal(const std::string& data){
     return result;
 }
 
+extern "C"{
+    EMSCRIPTEN_KEEPALIVE
+    int parseCSV(const char* csvData){
+        std::string data(csvData);
+        auto parsed=parseCSVInternal(data);
+        return parsed.size();
+    }
+    
+}
