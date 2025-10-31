4d06e7afb59715090d7c3e08f5fac82e6d27cc3c
Calum Kerr <calumxkerr@gmail.com>
2025-10-31 13:58:05 +0000

    Sorting out webassembly script for later, may need to change when script are updated

---
+++ build-wasm.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+EMCC="emsdk/upstream/emscripten/emcc"
+$EMCC src/algorithms.cpp\
+    -o frontend/algorithms.js\
+    -s WASM=1 \
+    -s EXPORTED_FUNCTIONS='["_parseCSV","_detectMissing","_detectDuplicates","_cleanData"]' \
+    -s EXPORTED_RUNTIME_METHODS='["ccall","cwrap"]' \
+    -s MODULARIZE=1 \
+    -s EXPORT_NAME='AlgorithmsModule' \
+    --std=c++17
