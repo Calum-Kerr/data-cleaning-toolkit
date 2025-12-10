215854b813e737e725ed51b89fa6b1b3b8b7099c
Calum Kerr <calumxkerr@gmail.com>
2025-12-10 16:40:47 +0000

    New route for to upper

--- src/main.cpp
+++ src/main.cpp
@@ -172,6 +172,11 @@ int main(){
         result["mode"]="api";
         return result;
     });
+
+    CROW_ROUTE(app,"/api/to-uppercase").methods("POST"_method)
+    ([&cleaner](const crow::request& req){
+        
+    });
     
     CROW_ROUTE(app,"/favicon.ico")
     ([](){
