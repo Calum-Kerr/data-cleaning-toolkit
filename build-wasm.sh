5d243f90b4eaf4a27623564af7053672dbd37d7d
Calum Kerr <calumxkerr@gmail.com>
2025-12-19 14:22:34 +0000

    Add outlier detection function to WASM build

--- build-wasm.sh
+++ build-wasm.sh
@@ -3,7 +3,7 @@ EMCC="emsdk/upstream/emscripten/emcc"
 $EMCC src/algorithms-wasm.cpp\
     -o frontend/algorithms.js\
     -s WASM=1 \
-    -s EXPORTED_FUNCTIONS='["_parseCSV","_detectMissing","_detectDuplicates","_cleanData","_cleanDataString","_detectWhitespace","_trimWhitespaceString","_toUpperCaseString","_toLowerCaseString","_detectNullValues","_standardiseNullValueString"]' \
+    -s EXPORTED_FUNCTIONS='["_parseCSV","_detectMissing","_detectDuplicates","_cleanData","_cleanDataString","_detectWhitespace","_trimWhitespaceString","_toUpperCaseString","_toLowerCaseString","_detectNullValues","_standardiseNullValueString","_detectOutliers"]' \
     -s EXPORTED_RUNTIME_METHODS='["ccall","cwrap"]' \
     -s MODULARIZE=1 \
     -s EXPORT_NAME='AlgorithmsModule' \
