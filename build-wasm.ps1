0c21ab2db661004109c17562fe24319ec94d44ad
Calum Kerr <calumxkerr@gmail.com>
2026-01-01 14:57:36 +0000

    Add _detectDataTypes function to exported WASM functions

--- build-wasm.ps1
+++ build-wasm.ps1
@@ -5,7 +5,7 @@
 emcc src/algorithms-wasm.cpp `
     -o frontend/algorithms.js `
     -s WASM=1 `
-    -s EXPORTED_FUNCTIONS='["_parseCSV","_detectMissing","_detectDuplicates","_cleanData","_cleanDataString","_detectWhitespace","_trimWhitespaceString","_toUpperCaseString","_toLowerCaseString","_detectNullValues","_standardiseNullValueString","_detectOutliers","_removeOutliersString","_detectInconsistentValues","_standardiseValuesString"]' `
+    -s EXPORTED_FUNCTIONS='["_parseCSV","_detectMissing","_detectDuplicates","_cleanData","_cleanDataString","_detectWhitespace","_trimWhitespaceString","_toUpperCaseString","_toLowerCaseString","_detectNullValues","_standardiseNullValueString","_detectOutliers","_removeOutliersString","_detectInconsistentValues","_standardiseValuesString","_detectDataTypes"]' `
     -s EXPORTED_RUNTIME_METHODS='["ccall","cwrap"]' `
     -s MODULARIZE=1 `
     -s EXPORT_NAME='AlgorithmsModule' `
