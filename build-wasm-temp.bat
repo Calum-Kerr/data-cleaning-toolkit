ff6020a3891a729f49c760846d13058bb743254f
Calum Kerr <calumxkerr@gmail.com>
2026-01-01 15:09:00 +0000

    Add WASM export for detectDataTypes function and update build script

---
+++ build-wasm-temp.bat
@@ -0,0 +1,5 @@
+@echo off
+call emsdk\emsdk_env.bat
+emcc src/algorithms-wasm.cpp -o frontend/algorithms.js -s WASM=1 -s EXPORTED_FUNCTIONS="[""_parseCSV"",""_detectMissing"",""_detectDuplicates"",""_cleanData"",""_cleanDataString"",""_detectWhitespace"",""_trimWhitespaceString"",""_toUpperCaseString"",""_toLowerCaseString"",""_detectNullValues"",""_standardiseNullValueString"",""_detectOutliers"",""_removeOutliersString"",""_detectInconsistentValues"",""_standardiseValuesString"",""_detectDataTypes""]" -s EXPORTED_RUNTIME_METHODS="[""ccall"",""cwrap""]" -s MODULARIZE=1 -s EXPORT_NAME="AlgorithmsModule" --std=c++17
+echo WASM build complete!
+
