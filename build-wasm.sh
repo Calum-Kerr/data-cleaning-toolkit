e98e662464b5c12123fd6a65e4e6db238e69a32c
Calum Kerr <calumxkerr@gmail.com>
2026-01-01 15:14:05 +0000

    Add _detectPatterns to EXPORTED_FUNCTIONS in WASM build script

--- build-wasm.sh
+++ build-wasm.sh
@@ -3,7 +3,7 @@ source emsdk/emsdk_env.sh
 emcc src/algorithms-wasm.cpp\
     -o frontend/algorithms.js\
     -s WASM=1 \
-    -s EXPORTED_FUNCTIONS='["_parseCSV","_detectMissing","_detectDuplicates","_cleanData","_cleanDataString","_detectWhitespace","_trimWhitespaceString","_toUpperCaseString","_toLowerCaseString","_detectNullValues","_standardiseNullValueString","_detectOutliers","_removeOutliersString","_detectInconsistentValues","_standardiseValuesString","_detectDataTypes","_standardiseDateColumnString","_standardiseNumberColumnString","_detectFileEncoding"]' \
+    -s EXPORTED_FUNCTIONS='["_parseCSV","_detectMissing","_detectDuplicates","_cleanData","_cleanDataString","_detectWhitespace","_trimWhitespaceString","_toUpperCaseString","_toLowerCaseString","_detectNullValues","_standardiseNullValueString","_detectOutliers","_removeOutliersString","_detectInconsistentValues","_standardiseValuesString","_detectDataTypes","_standardiseDateColumnString","_standardiseNumberColumnString","_detectFileEncoding","_detectPatterns"]' \
     -s EXPORTED_RUNTIME_METHODS='["ccall","cwrap"]' \
     -s MODULARIZE=1 \
     -s EXPORT_NAME='AlgorithmsModule' \
