d3a5c269f600f11d3942fe382199089ae1a5333f
Calum Kerr <calumxkerr@gmail.com>
2026-01-29 15:20:18 +0000

    Add build-wasm.bat script for WASM compilation setup

---
+++ build-wasm.bat
@@ -0,0 +1,26 @@
+@echo off
+setlocal enabledelayedexpansion
+
+REM Set up the environment
+set EMSDK=%CD%\emsdk
+set PATH=%EMSDK%\upstream\emscripten;%EMSDK%\upstream\bin;%EMSDK%\node\22.16.0_64bit\bin;%PATH%
+
+REM Run emcc
+%EMSDK%\upstream\emscripten\emcc.bat src/algorithms-wasm.cpp ^
+    -o frontend/algorithms.js ^
+    -s WASM=1 ^
+    -s "EXPORTED_FUNCTIONS=[_parseCSV,_detectMissing,_detectDuplicates,_cleanData,_cleanDataString,_detectWhitespace,_trimWhitespaceString,_toUpperCaseString,_toLowerCaseString,_detectNullValues,_standardiseNullValueString,_detectOutliers,_removeOutliersString,_detectInconsistentValues,_standardiseValuesString,_detectDataTypes,_standardiseDateColumnString,_standardiseNumberColumnString,_detectFileEncoding,_detectPatterns,_autoDetectAll,_measurePerformance,_profileColumn]" ^
+    -s "EXPORTED_RUNTIME_METHODS=[ccall,cwrap]" ^
+    -s MODULARIZE=1 ^
+    -s EXPORT_NAME=AlgorithmsModule ^
+    --std=c++17 ^
+    -fstack-protector-strong ^
+    -D_FORTIFY_SOURCE=2 ^
+    -Wformat -Wformat-security ^
+    -fPIE ^
+    -s ALLOW_MEMORY_GROWTH=1 ^
+    -s MAXIMUM_MEMORY=2GB ^
+    -s ABORTING_MALLOC=1
+
+endlocal
+
