a88b2d37120cb185fbabd4679b1995087c3d5538
Calum Kerr <calumxkerr@gmail.com>
2026-02-02 16:27:31 +0000

    Add _extractFirstTokenString to exported functions for enhanced CSV parsing

--- build-wasm.ps1
+++ build-wasm.ps1
@@ -5,7 +5,7 @@
 emcc src/algorithms-wasm.cpp `
     -o frontend/algorithms.js `
     -s WASM=1 `
-    -s EXPORTED_FUNCTIONS='["_parseCSV","_detectMissing","_detectDuplicates","_cleanData","_cleanDataString","_detectWhitespace","_trimWhitespaceString","_toUpperCaseString","_toLowerCaseString","_detectNullValues","_standardiseNullValueString","_detectOutliers","_removeOutliersString","_detectInconsistentValues","_standardiseValuesString","_detectDataTypes","_standardiseDateColumnString","_standardiseNumberColumnString","_detectFileEncoding","_detectPatterns","_autoDetectAll","_measurePerformance"]' `
+    -s EXPORTED_FUNCTIONS='["_parseCSV","_detectMissing","_detectDuplicates","_cleanData","_cleanDataString","_detectWhitespace","_trimWhitespaceString","_toUpperCaseString","_toLowerCaseString","_detectNullValues","_standardiseNullValueString","_detectOutliers","_removeOutliersString","_detectInconsistentValues","_standardiseValuesString","_detectDataTypes","_standardiseDateColumnString","_standardiseNumberColumnString","_detectFileEncoding","_detectPatterns","_autoDetectAll","_measurePerformance","_extractFirstTokenString"]' `
     -s EXPORTED_RUNTIME_METHODS='["ccall","cwrap"]' `
     -s MODULARIZE=1 `
     -s EXPORT_NAME='AlgorithmsModule' `
