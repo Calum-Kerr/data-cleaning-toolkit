0b0cf7f802eb1677d5b448dee3069968e6e949de
Calum Kerr <calumxkerr@gmail.com>
2026-02-02 16:31:17 +0000

    Remove Levenshtein distance and similarity calculation functions from WASM build

--- src/algorithms-wasm.cpp
+++ src/algorithms-wasm.cpp
@@ -901,31 +901,6 @@ extern "C"{
         return cstr;
     }
 
-    int levenshteinDistance(const std::string& s1, const std::string& s2){
-        size_t len1=s1.length();
-        size_t len2=s2.length();
-        std::vector<std::vector<int>> dp(len1+1,std::vector<int>(len2+1,0));
-        for(size_t i=0;i<=len1;++i)dp[i][0]=i;
-        for(size_t j=0;j<=len2;++j)dp[0][j]=j;
-        for(size_t i=1;i<=len1;++i){
-            for(size_t j=1;j<=len2;++j){
-                if(s1[i-1]==s2[j-1]){
-                    dp[i][j]=dp[i-1][j-1];
-                }else{
-                    dp[i][j]=1+std::min({dp[i-1][j],dp[i][j-1],dp[i-1][j-1]});
-                }
-            }
-        }
-        return dp[len1][len2];
-    }
-
-    double calculateSimilarity(const std::string& s1, const std::string& s2){
-        int distance=levenshteinDistance(s1,s2);
-        int maxLen=std::max(s1.length(),s2.length());
-        if(maxLen==0)return 1.0;
-        return 1.0-(double)distance/maxLen;
-    }
-
     EMSCRIPTEN_KEEPALIVE
     const char* extractFirstTokenString(const char* csvData){
         std::string data(csvData);
