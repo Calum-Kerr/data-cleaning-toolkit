bb9bdcaee3d61a3608906981f33c81eda4de6cc4
Calum-Kerr <calumxkerr@gmail.com>
2026-03-24 20:48:08 +0000

    Add removeStateSuffixes function

--- src/text_utils2.cpp
+++ src/text_utils2.cpp
@@ -1,4 +1,32 @@
 #include "text_utils.h"
+#include <algorithm>
 
-// Additional text utilities
+std::string removeStateSuffixes(const std::string& text){
+  std::string result=text;
+  size_t commaPos=result.rfind(',');
+  if(commaPos!=std::string::npos) result=result.substr(0,commaPos);
+  std::string upper=result;
+  std::transform(upper.begin(),upper.end(),upper.begin(),::toupper);
+  const std::vector<std::string> patterns={
+    " ALABAMA"," ALASKA"," ARIZONA"," ARKANSAS"," CALIFORNIA"," COLORADO",
+    " CONNECTICUT"," DELAWARE"," FLORIDA"," GEORGIA"," HAWAII"," IDAHO",
+    " ILLINOIS"," INDIANA"," IOWA"," KANSAS"," KENTUCKY"," LOUISIANA",
+    " MAINE"," MARYLAND"," MASSACHUSETTS"," MICHIGAN"," MINNESOTA",
+    " MISSISSIPPI"," MISSOURI"," MONTANA"," NEBRASKA"," NEVADA",
+    " NEW HAMPSHIRE"," NEW JERSEY"," NEW MEXICO"," NORTH CAROLINA",
+    " NORTH DAKOTA"," OHIO"," OKLAHOMA"," OREGON"," PENNSYLVANIA",
+    " RHODE ISLAND"," SOUTH CAROLINA"," SOUTH DAKOTA"," TENNESSEE"," TEXAS",
+    " UTAH"," VERMONT"," VIRGINIA"," WEST VIRGINIA",
+    " WISCONSIN"," WYOMING"," DISTRICT OF COLUMBIA"," US"," USA"," UNITED STATES"
+  };
+  for(const auto& p:patterns){
+    size_t pos=upper.rfind(p);
+    if(pos!=std::string::npos && pos+p.length()==upper.length()){
+      result=result.substr(0,pos);
+      upper=upper.substr(0,pos);
+    }
+  }
+  while(!result.empty() && (result.back()==' '||result.back()=='\t')) result.pop_back();
+  return result;
+}
 
