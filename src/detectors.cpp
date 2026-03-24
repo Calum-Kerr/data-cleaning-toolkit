4a430ab1e4e45a2c86d2f1ea4370c9b2f1d3b401
Calum-Kerr <calumxkerr@gmail.com>
2026-03-24 20:48:27 +0000

    Add levenshteinDistance function

---
+++ src/detectors.cpp
@@ -0,0 +1,19 @@
+#include "detectors.h"
+#include <algorithm>
+#include <set>
+
+int levenshteinDistance(const std::string& s1, const std::string& s2){
+  size_t m=s1.length();
+  size_t n=s2.length();
+  std::vector<std::vector<int>> dp(m+1,std::vector<int>(n+1,0));
+  for(size_t i=0;i<=m;++i) dp[i][0]=i;
+  for(size_t j=0;j<=n;++j) dp[0][j]=j;
+  for(size_t i=1;i<=m;++i){
+    for(size_t j=1;j<=n;++j){
+      if(s1[i-1]==s2[j-1]) dp[i][j]=dp[i-1][j-1];
+      else dp[i][j]=1+std::min({dp[i-1][j],dp[i][j-1],dp[i-1][j-1]});
+    }
+  }
+  return dp[m][n];
+}
+
