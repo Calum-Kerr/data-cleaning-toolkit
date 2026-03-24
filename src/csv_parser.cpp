0be5a1f6292db740578e18f305f360f7a8b54365
Calum-Kerr <calumxkerr@gmail.com>
2026-03-24 20:46:28 +0000

    Add parseCSVLine initialization

--- src/csv_parser.cpp
+++ src/csv_parser.cpp
@@ -1,6 +1,9 @@
 #include "csv_parser.h"
 
 std::vector<std::string> parseCSVLine(const std::string& line){
-  return {};
+  std::vector<std::string> row;
+  std::string cell;
+  bool inQuotes=false;
+  return row;
 }
 
