d99ee9762e13fc25b6d4bf743bf5c75cba13e477
Calum-Kerr <calumxkerr@gmail.com>
2026-03-25 19:54:57 +0000

    routes_frontend.cpp: Add getContentType function part 1 (7 lines)

--- src/routes_frontend.cpp
+++ src/routes_frontend.cpp
@@ -20,5 +20,13 @@ std::string readFile(const std::string& filepath) {
   return buffer.str();
 }
 
+std::string getContentType(const std::string& filepath) {
+  if (endsWith(filepath, ".html")) return "text/html; charset=utf-8";
+  if (endsWith(filepath, ".css")) return "text/css; charset=utf-8";
+  if (endsWith(filepath, ".js")) return "application/javascript; charset=utf-8";
+  if (endsWith(filepath, ".json")) return "application/json; charset=utf-8";
+  if (endsWith(filepath, ".wasm")) return "application/wasm";
+
+
 
 
