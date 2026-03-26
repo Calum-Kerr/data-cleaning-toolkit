36537660a9b1178e5b4a0b9c9c7a9b1b3910ef46
Calum-Kerr <calumxkerr@gmail.com>
2026-03-26 16:25:51 +0000

    Add daily summary alert logging

--- src/alerts.cpp
+++ src/alerts.cpp
@@ -5,3 +5,8 @@ void writeStartupAlert() {
   std::ofstream alert("/tmp/toolkit_alerts.log", std::ios::app);
   alert << "APP STARTED\n";
 }
+
+void writeDailySummary(int requests, int errors) {
+  std::ofstream alert("/tmp/toolkit_alerts.log", std::ios::app);
+  alert << "DAILY: " << requests << " requests, " << errors << " errors\n";
+}
