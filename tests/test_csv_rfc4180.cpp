d1130fb88c2b47e69da721f75b776615a5ae7396
Calum-Kerr <calumxkerr@gmail.com>
2026-03-27 23:34:54 +0000

    Expand test harness with RFC 4180 compliance cases

--- tests/test_csv_rfc4180.cpp
+++ tests/test_csv_rfc4180.cpp
@@ -1,10 +1,17 @@
 #include "../include/csv_parser.h"
 #include <cassert>
+#include <iostream>
 
 int main(){
-auto simple=parseCSVRFC4180("a,b,c");
-assert(simple[0][0]=="a"&&simple[0][1]=="b"&&simple[0][2]=="c");
-auto quoted=parseCSVRFC4180("\"x\",\"y\",\"z\"");
-assert(quoted[0][0]=="x"&&quoted[0][1]=="y"&&quoted[0][2]=="z");
+std::cout<<"Testing RFC 4180 parser..."<<std::endl;
+auto t1=parseCSVRFC4180("a,b,c");
+assert(t1.size()==1&&t1[0].size()==3);
+auto t2=parseCSVRFC4180("\"x\",\"y\",\"z\"");
+assert(t2[0][0]=="x"&&t2[0][1]=="y"&&t2[0][2]=="z");
+auto t3=parseCSVRFC4180("\"a,b\",c");
+assert(t3[0][0]=="a,b"&&t3[0][1]=="c");
+auto t4=parseCSVRFC4180("\"a\"\"b\",c");
+assert(t4[0][0]=="a\"b");
+std::cout<<"All tests passed"<<std::endl;
 return 0;
 }
