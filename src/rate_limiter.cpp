e1abc913b2ea3e78b620d03b702b86bb3074204e
Calum-Kerr <calumxkerr@gmail.com>
2026-03-30 14:31:45 +0100

    Add time window constant and reset timestamp tracking

--- src/rate_limiter.cpp
+++ src/rate_limiter.cpp
@@ -2,7 +2,10 @@
 #include <map>
 #include <chrono>
 
+const int WINDOW_SECONDS=60;
+const int MAX_REQUESTS=100;
 std::map<std::string, int> requestCounts;
+std::map<std::string, std::chrono::steady_clock::time_point> lastReset;
 
 bool checkRateLimit(const std::string& ip) {
   requestCounts[ip]++;
