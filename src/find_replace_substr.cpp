f34aaeb5cf46f953cb734e1e1d85526ffb7fb05d
Calum-Kerr <calumxkerr@gmail.com>
2026-03-31 01:03:24 +0100

    Add case-insensitive substring replacement logic

---
+++ src/find_replace_substr.cpp
@@ -0,0 +1,33 @@
+#include "find_replace.h"
+#include <algorithm>
+#include <cctype>
+
+static std::string lowerCaseStr(const std::string& s) {
+  std::string r = s;
+  std::transform(r.begin(), r.end(), r.begin(), [](unsigned char c) {
+    return std::tolower(c);
+  });
+  return r;
+}
+
+std::string applySubstringReplace(const std::string& cell,
+  const FindReplaceRule& rule) {
+  std::string find = rule.caseSensitive ? rule.find : lowerCaseStr(rule.find);
+  std::string lower = lowerCaseStr(cell);
+  if(!rule.caseSensitive) {
+    std::string res;
+    size_t last = 0, pos = 0;
+    while((pos = lower.find(find, pos)) != std::string::npos) {
+      res += cell.substr(last, pos - last) + rule.replace;
+      last = pos + find.length();
+      pos += find.length();
+    }
+    return res + cell.substr(last);
+  }
+  std::string cellCopy = cell;
+  for(size_t pos = 0; (pos = cellCopy.find(rule.find, pos)) != std::string::npos;) {
+    cellCopy.replace(pos, rule.find.length(), rule.replace);
+    pos += rule.replace.length();
+  }
+  return cellCopy;
+}
