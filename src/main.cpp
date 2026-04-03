fb1535a82a843490bcc6460e33e6ba973fa01c23
Calum-Kerr <calumxkerr@gmail.com>
2026-04-03 16:07:17 +0100

    Fix variable name collision in /api/clean endpoint

--- src/main.cpp
+++ src/main.cpp
@@ -62,27 +62,27 @@ int main(){
     int rowsAfterStandard=standardised.size();
     auditLog.addEntry("Remove Duplicates", 0, rowsAfterStandard, rowsAfterStandard);
     auto cleaned=removeDuplicates(standardised);
-    std::string csvData;
-    csvData.reserve(req.body.size());
+    std::string outputCsv;
+    outputCsv.reserve(req.body.size());
     for(const auto& row:cleaned){
       for(size_t j=0;j<row.size();j++){
-        if(j>0)csvData+=",";
+        if(j>0)outputCsv+=",";
         const auto& cell=row[j];
         bool needsQuote=cell.find(',')!=std::string::npos||cell.find('"')!=std::string::npos||cell.find('\n')!=std::string::npos;
         if(needsQuote){
-          csvData+="\"";
+          outputCsv+="\"";
           for(char c:cell){
-            if(c=='"')csvData+="\"\"";
-            else csvData+=c;
+            if(c=='"')outputCsv+="\"\"";
+            else outputCsv+=c;
           }
-          csvData+="\"";
-        }else csvData+=cell;
+          outputCsv+="\"";
+        }else outputCsv+=cell;
       }
-      csvData+="\r\n";
+      outputCsv+="\r\n";
     }
     std::string jsonBody="{\"cleanedData\":\"";
-    jsonBody.reserve(csvData.size()*2);
-    for(char c:csvData){
+    jsonBody.reserve(outputCsv.size()*2);
+    for(char c:outputCsv){
       if(c=='\\')jsonBody+="\\\\";
       else if(c=='"')jsonBody+="\\\"";
       else if(c=='\n')jsonBody+="\\n";
